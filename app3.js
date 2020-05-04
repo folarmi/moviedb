d2e5049e48ef7495005f43c0457ea9c1af0637dd
folarmi <fola4ril@gmail.com>
2020-05-03 18:08:00 -0700

    fixed image link

--- app3.js
+++ app3.js
@@ -10,7 +10,7 @@ function showAllMovies() {
 let pictureSize;
 const width = screen.width;
 
-if (width <= 375 || width >= 1023){
+if (width <= 375){
     pictureSize = "w185"
 } else if (width === 1024 || width > 1024){
     pictureSize = "w500"
